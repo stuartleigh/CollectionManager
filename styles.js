dacdf150855bc16f2119f12b92823189cb862eeb
Stuart <stuart.leigh83@gmail.com>
2015-05-11 09:35:56 +0100

    removing some styles

--- styles.js
+++ styles.js
@@ -6,23 +6,20 @@ var {
 var Container = StyleSheet.create({
   Container: {
     flex: 1,
-    backgroundColor: '#1F2636',
+    backgroundColor: '#ffffff',
   },
 });
 
 var CardList = StyleSheet.create({
   Container: {
     flex: 1,
-    backgroundColor: '#1F2636',
-    borderColor: '#1F2636',
   },
   SectionHeader: {
     flex: 1,
     padding: 10,
     borderBottomColor: '#000000',
     borderBottomWidth: 1,
-    borderTopColor: '#3C4866',
-    borderTopWidth: 1,
+    backgroundColor: '#1F2636',
   },
   SectionHeader__Text: {
     textAlign: 'center',
@@ -37,42 +34,32 @@ var Card = StyleSheet.create({
     flexDirection: 'row',
     borderBottomColor: '#000000',
     borderBottomWidth: 1,
-    borderTopColor: '#3C4866',
-    borderTopWidth: 1,
   },
   'Container--deactive': {
     flex: 1,
     flexDirection: 'row',
     borderBottomColor: '#000000',
     borderBottomWidth: 1,
-    borderTopColor: '#3C4866',
-    borderTopWidth: 1,
-    backgroundColor: '#cccccc',
   },
   'ManaCost--free': {
     padding: 12,
     width: 44,
-    backgroundColor: '#FEFFF8',
   },
   'ManaCost--common': {
     padding: 12,
     width: 44,
-    backgroundColor: '#B7C2BA',
   },
   'ManaCost--rare': {
     padding: 12,
     width: 44,
-    backgroundColor: '#3D74FF',
   },
   'ManaCost--epic': {
     padding: 12,
     width: 44,
-    backgroundColor: '#8746FF',
   },
   'ManaCost--legendary': {
     padding: 12,
     width: 44,
-    backgroundColor: '#FF872F',
   },
   ManaCost__Cost: {
     textAlign: 'center',
@@ -80,30 +67,27 @@ var Card = StyleSheet.create({
   Name: {
     flex: 1,
     padding: 10,
-    borderRightColor: '#000000',
-    borderRightWidth: 1,
-    borderLeftWidth: 1,
   },
   Name__Name: {
-    color: '#ffffff',
-    fontWeight: 'bold',
   },
   NormalCount: {
     padding: 12,
     width: 44,
+    borderColor: '#000000',
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
   },
   NormalCount__Count: {
     textAlign: 'center',
-    color: '#ffffff',
     fontWeight: 'bold',
   },
   GoldCount: {
     padding: 12,
     width: 44,
-    backgroundColor: '#FFD938',
   },
   GoldCount__Count: {
     textAlign: 'center',
+    fontWeight: 'bold',
   },
 })
 
